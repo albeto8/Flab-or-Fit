b40619ca1fe8d57258a36832f81e72a81037d60b
Mario Barragán <albeto8@gmail.com>
2017-03-29 17:59:29 -0600

    OnclickListeners without Buttons

--- app/src/main/java/com/example/mariobarragan/flaborfit/MainActivity.java
+++ app/src/main/java/com/example/mariobarragan/flaborfit/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.mariobarragan.flaborfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXCERCISE_WEIGHTS = "Weight lifting";
+    public static final String EXCERCISE_YOGA = "Yoga";
+    public static final String EXCERCISE_CARDIO = "Cardio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout weightBtn = (RelativeLayout)findViewById(R.id.weightBtn);
+        RelativeLayout yogaBtn = (RelativeLayout)findViewById(R.id.yogaBtn);
+        RelativeLayout cardioBtn = (RelativeLayout)findViewById(R.id.cardioBtn);
+
+        weightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(MainActivity.EXCERCISE_WEIGHTS);
+            }
+        });
+
+        yogaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(MainActivity.EXCERCISE_YOGA);
+            }
+        });
+
+        cardioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(MainActivity.EXCERCISE_CARDIO);
+            }
+        });
+    }
+
+    private void loadDetailActivity(String excerciseTitle) {
+
     }
 }
