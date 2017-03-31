44564a3eae2f542ff7c2ee43d5f59405f9e4c473
Mario Barragán <albeto8@gmail.com>
2017-03-31 17:50:37 -0600

    Intents & Extras

--- app/src/main/java/com/example/mariobarragan/flaborfit/DetailsActivity.java
+++ app/src/main/java/com/example/mariobarragan/flaborfit/DetailsActivity.java
@@ -1,8 +1,11 @@
 package com.example.mariobarragan.flaborfit;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -12,7 +15,21 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        TextView exerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
+        TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);
         ImageView exerciseImage = (ImageView)findViewById(R.id.excerciseIimage);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBG);
+        String excerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        exerciseText.setText(excerciseTitle);
+
+        if (excerciseTitle.equalsIgnoreCase(MainActivity.EXCERCISE_WEIGHTS)) {
+            exerciseImage.setImageDrawable(getDrawable(R.drawable.weight));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        } else if (excerciseTitle.equalsIgnoreCase(MainActivity.EXCERCISE_CARDIO)) {
+            mainBG.setBackgroundColor(Color.parseColor("#528d56"));
+            exerciseImage.setImageDrawable(getDrawable(R.drawable.heart));
+        } else {
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+            exerciseImage.setImageDrawable(getDrawable(R.drawable.lotus));
+        }
     }
 }
